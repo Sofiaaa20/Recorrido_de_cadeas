a17357e45afa87805388f33e88c5682230378d66
sofia ramirez <sofaiaaa20@gmail.com>
2023-06-28 10:00:18 -0500

    Taller recorrido de cadenas

---
+++ 1.js
@@ -0,0 +1,14 @@
+/*1.Dada la cadena “Javascript es un buen lenguaje”:
+a) Leer e imprimir los elementos de índices 3, 6, 8
+b) Recorra la cadena usando ciclo for */
+
+let cadena = "Javascript es un buen lenguaje";
+
+console.log(cadena[3],cadena[6],cadena[8]);
+
+
+for (let index = 0; index < cadena.length; index++) {
+    console.log(cadena[index]);
+    
+    
+}
